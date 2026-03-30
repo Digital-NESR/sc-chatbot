d6dfda89f19d5d3146313fdc4ca626c0df600044
farhan_2312 <farhanmohammed2312@gmail.com>
2026-03-30 16:18:56 +0400

    Fixing Route errors

--- src/app/api/chat/message/route.ts
+++ src/app/api/chat/message/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../../auth/[...nextauth]/route';
 import { prisma } from '@/lib/prisma';
+export const dynamic = 'force-dynamic';
 
 export async function POST(request: Request) {
     const session = await getServerSession(authOptions);
